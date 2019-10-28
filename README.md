eddcc8c44fc90407cb86562d3601bc52c43d5c9c
Onyinye Ezike <ezikeonyinyefavour@gmail.com>
2019-10-28 16:30:52 +0100

    (add firebase functions): add firebase functions
    - add firebase functions

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # firebase_functions
 Leverage Firebase Functions in order to persist the user details in the Realtime Database.
+
+## Getting started
+There is no need to set up or scale a server, we will just write the functions and deploy them to Firebase. They will be triggered only when the requests are called.
+
+At this very moment, Google Cloud Functions can be written in Node.js (v6 or v8), Python (beta) or Go (beta). I will proceed further with Node.js and some additional resources such as Express and CORS.
---
+++ functions/index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const cors = require('cors');
+const uuid = require('uuid/v5');
+
+// Firebase init
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// Express and CORS middleware init
+
+const app = express();
+app.use(cors());
+
+
+// app.use((request, response, next) => {
+//     request.header("Access-Control-Allow-Origin", "*");
+//     response.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+// });
+
+
+const user_id = (data) => {
+    const namespace = `${data}`;
+
+    return uuid(namespace, uuid.DNS);
+};
+
+app.post("/", async (request, response) => {
+    try {
+        const {
+            first_name,
+            last_name,
+            birth_day,
+            age,
+            hobby
+        } = request.body;
+        await admin.database().ref('/users').push({
+            first_name,
+            last_name,
+            birth_day,
+            age,
+            hobby
+        });
+        return response.status(200).send({
+            first_name,
+            last_name,
+            birth_day,
+            age,
+            hobby
+        })
+    } catch (error) {
+        return response.status(500).send('Something went wrong: ' + error.message);
+    }
+});
+
+app.get("/", (request, response) => {
+    return admin.database().ref().on("value", snapshot => {
+        return response.status(200).send(snapshot.val());
+    }, error => {
+        return response.status(500).send('Something went wrong: ' + error.message);
+    });
+});
+
+exports.users = functions.https.onRequest(app);
+
+exports.addUserId = functions.database.ref('/users/{pushID}')
+    .onCreate((snapshot) => {
+        const formData = snapshot.val();
+
+        return snapshot.ref.update({
+            id: user_id(formData['birth_day']),
+        });
+    });
